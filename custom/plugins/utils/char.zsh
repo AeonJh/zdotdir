f78e86bb824911f243bb5d6d813877a5f77918b0
AeonJh Fu <aeonjh@nanoleaf.me>
2024-08-27 10:45:20 +0800

    chg: make Usage: h2d <hex_number>... Usage: d2h <decimal_number>... case-insensitive and support arguments.

--- custom/plugins/utils/char.zsh
+++ custom/plugins/utils/char.zsh
@@ -3,23 +3,28 @@
 # 1. h2d: Convert hex to decimal
 # 2. d2h: Convert decimal to hex
 h2d(){
-  usage="Usage: h2d <hex_number>"
+  usage="Usage: h2d <hex_number>..."
   # check if hex number is provided
   if [[ -z "$1" ]]; then
     echo $usage
     return
   fi
-  echo "ibase=16; $@"|bc
+  for arg in "$@"; do
+    # echo -n "$(echo "ibase=16; ${arg^^}"|bc) " # uppercase ${arg^^} is for bash 4.0+, unsupported for zsh
+    echo -n "$(echo "ibase=16; ${(U)arg}"|bc) "
+  done
 }
 
 d2h(){
-  usage="Usage: d2h <decimal_number>"
+  usage="Usage: d2h <decimal_number>..."
   # check if decimal number is provided
   if [[ -z "$1" ]]; then
     echo $usage
     return
   fi
-  echo "obase=16; $@"|bc
+  for arg in "$@"; do
+    echo -n "$(echo "obase=16; $arg"|bc) "
+  done
 }
 
 # 3. display various color codes
